62f9f6731905be677674d1f4c3dbb8788ef40fb5
Marcos Ameneiros Pérez <marcosap10100@gmail.com>
2025-11-05 15:22:36 +0100

    Exercicio 30 OK

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -1,7 +1,25 @@
 package com.campusdual.classroom;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class Exercise {
     public static void main(String[] args) {
+        InputStream is = Exercise.class.getClassLoader().getResourceAsStream("lorem.txt");
+        if (is == null) {
+            System.out.println("Error al cargar el fichero.");
+            return;
+        }
 
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el fichero: " + e.getMessage());
+        }
     }
 }
